4171e96a23a3d7f278872723034c05be00ccd5ab
Ihor Melnyk <igor.melnick85@gmail.com>
2023-08-21 14:18:40 +0300

    finish code

--- Договір інспектування локомотивів.js
+++ Договір інспектування локомотивів.js
@@ -1,5 +1,3 @@
-//Скрипт 1. Зміна властивостей атрибутів
-
 function setControlRequired(code, required = true) {
   const control = EdocsApi.getControlProperties(code);
   control.required = required;
@@ -21,6 +19,7 @@ function setValueAttr(code, value) {
   EdocsApi.setAttributeValue(attr);
 }
 
+//Скрипт 1. Зміна властивостей атрибутів
 function onChangeCheckBranch(clearOnInit = true) {
   if (clearOnInit) {
   } else {
@@ -104,6 +103,9 @@ function onChangeCheckBranch(clearOnInit = true) {
   }
 }
 
+function onCardInitialize() {
+  onChangeCheckBranch();
+}
 //Скрипт 2. Вирахування ПДВ
 function calculationOfSums() {
   let VATpercentage = 1;
@@ -185,7 +187,7 @@ function onChangeNumberServices() {
 
 //Скрипт 4. Заповнення значення поля вартості послуг та суми ПДВ прописом
 function setAmountDescription() {
-  const attrServicesAmount = EdocsApi.getAttributeValue("ServicesAmount");
+  const { attrServicesAmount } = calculationOfSums();
   const attrServicesVATAmount = EdocsApi.getAttributeValue("ServicesVATAmount");
 
   if (attrServicesAmount.value) {
@@ -203,86 +205,105 @@ function setAmountDescription() {
   }
 }
 
-//Скрипт 6. Заповнення інформації про додаткового підписанта
-function onChangeOrgAgentSurname2() {
-  setOrgAgentInformation();
-}
-
-function setOrgAgentInformation() {
-  const attrOrgAgentSurname2 = EdocsApi.getAttributeValue("OrgAgentSurname2");
-  if (attrOrgAgentSurname2.value) {
-    const data = EdocsApi.getAttributeValue("OrgAgentSurname2"); //по id контрагента зчитати інфу
-    setValueAttr("OrgAgent2", data.OrgAgent2);
-    setValueAttr("OrgAgentPosition2", data.OrgAgentPosition2);
-    setValueAttr("ActsOnBasisOrg2", data.ActsOnBasisOrg2);
-  }
-}
-
 //Скрипт 7. Передача договору на підписання в зовнішню систему
 function setDataForESIGN() {
-    debugger;
-    var registrationDate = EdocsApi.getAttributeValue('RegDate').value;
-    var registrationNumber = EdocsApi.getAttributeValue('RegNumber').value;
-    var caseType = EdocsApi.getAttributeValue('DocType').value;
-    var caseKind = EdocsApi.getAttributeValue('DocKind').text;
-    var name = '';
-    if (caseKind) {
-        name += caseKind;
-    } else {
-        name += caseType;
-    }
-    name += ' №' + (registrationNumber ? registrationNumber : CurrentDocument.id) + (!registrationDate ? '' : (' від ' + moment(registrationDate)
-        .format('DD.MM.YYYY')));
-    doc = {
-        DocName: name,
-        extSysDocId: CurrentDocument.id,
-        ExtSysDocVersion: CurrentDocument.version,
-        docType: "locomotiveInspectionContract",
-        docDate: registrationDate,
-        docNum: registrationNumber,
-        "File": "",
-        "parties": [
-            {
-                "taskType": "ToSign",
-                "taskState": "Done",
-                "legalEntityCode": EdocsApi.getAttributeValue('OrgCode').value,
-                "contactPersonEmail": EdocsApi.getAttributeValue('OrgRPEmail').value,
-                "signatures": []
-            },
-            {
-                "taskType": "ToSign",
-                "taskState": "NotAssigned",
-                "legalEntityCode": EdocsApi.getAttributeValue('ContractorEDRPOU').value,
-                "contactPersonEmail": EdocsApi.getAttributeValue('ContractorRPEmail').value,
-                "expectedSignatures": []
-            }],
-        "additionalAttributes": [
-            {
-                "code": "docDate",
-                "type": "dateTime",
-                "value": registrationDate
-            },
-            {
-                "code": "docNum",
-                "type": "string",
-                "value": registrationNumber
-            }
-        ],
-        sendingSettings: {
-            attachFiles: 'fixed', //, можна також встановлювати 'firstOnly' - Лише файл із першої зафіксованої вкладки(Головний файл), або 'all' - всі файли, 'fixed' - усі зафіксовані
-            attachSignatures: 'signatureAndStamp' // -'signatureAndStamp'Типи “Підпис” або “Печатка”, можна також встановити 'all' - усі типи цифрових підписів
-        }
-    }
-    EdocsApi.setAttributeValue({code: 'JSON', value: JSON.stringify(doc)});
+  debugger;
+  var registrationDate = EdocsApi.getAttributeValue("RegDate").value;
+  var registrationNumber = EdocsApi.getAttributeValue("RegNumber").value;
+  var caseType = EdocsApi.getAttributeValue("DocType").value;
+  var caseKind = EdocsApi.getAttributeValue("DocKind").text;
+  var name = "";
+  if (caseKind) {
+    name += caseKind;
+  } else {
+    name += caseType;
+  }
+  name += " №" + (registrationNumber ? registrationNumber : CurrentDocument.id) + (!registrationDate ? "" : " від " + moment(registrationDate).format("DD.MM.YYYY"));
+  doc = {
+    DocName: name,
+    extSysDocId: CurrentDocument.id,
+    ExtSysDocVersion: CurrentDocument.version,
+    docType: "locomotiveInspectionContract",
+    docDate: registrationDate,
+    docNum: registrationNumber,
+    File: "",
+    parties: [
+      {
+        taskType: "ToSign",
+        taskState: "Done",
+        legalEntityCode: EdocsApi.getAttributeValue("OrgCode").value,
+        contactPersonEmail: EdocsApi.getAttributeValue("OrgRPEmail").value,
+        signatures: [],
+      },
+      {
+        taskType: "ToSign",
+        taskState: "NotAssigned",
+        legalEntityCode: EdocsApi.getAttributeValue("CheckBranch ").value ? EdocsApi.getAttributeValue("BranchEDRPOU").value : EdocsApi.getAttributeValue("ContractorEDRPOU").value,
+        contactPersonEmail: EdocsApi.getAttributeValue("ContractorRPEmail").value,
+        expectedSignatures: [],
+      },
+    ],
+    additionalAttributes: [
+      {
+        code: "docDate",
+        type: "dateTime",
+        value: registrationDate,
+      },
+      {
+        code: "docNum",
+        type: "string",
+        value: registrationNumber,
+      },
+    ],
+    sendingSettings: {
+      attachFiles: "fixed", //, можна також встановлювати 'firstOnly' - Лише файл із першої зафіксованої вкладки(Головний файл), або 'all' - всі файли, 'fixed' - усі зафіксовані
+      attachSignatures: "signatureAndStamp", // -'signatureAndStamp'Типи “Підпис” або “Печатка”, можна також встановити 'all' - усі типи цифрових підписів
+    },
+  };
+  EdocsApi.setAttributeValue({ code: "JSON", value: JSON.stringify(doc) });
 }
 
 function onTaskExecuteSendOutDoc(routeStage) {
-    debugger;
-    if (routeStage.executionResult == 'rejected') {
-        return;
-    }
-    setDataForESIGN();
-        var idnumber = EdocsApi.getAttributeValue('DocId');
-        var methodData = {
-                extSysDocId: idnumber.value
-        };
+  debugger;
+  if (routeStage.executionResult == "rejected") {
+    return;
+  }
+  setDataForESIGN();
+  var idnumber = EdocsApi.getAttributeValue("DocId");
+  var methodData = {
+    extSysDocId: idnumber.value,
+  };
+
+  routeStage.externalAPIExecutingParams = {
+    externalSystemCode: "ESIGN1", // код зовнішньої системи
+    externalSystemMethod: "integration/importDoc", // метод зовнішньої системи
+    data: methodData, // дані, що очікує зовнішня система для заданого методу
+    executeAsync: true, // виконувати завдання асинхронно
+  };
+}
+
+function onTaskCommentedSendOutDoc(caseTaskComment) {
+  debugger;
+  var orgCode = EdocsApi.getAttributeValue("OrgCode").value;
+  var orgShortName = EdocsApi.getAttributeValue("OrgShortName").value;
+  if (!orgCode || !orgShortName) {
+    return;
+  }
+  var idnumber = EdocsApi.getAttributeValue("DocId");
+  var methodData = {
+    extSysDocId: idnumber.value,
+    eventType: "CommentAdded",
+    comment: caseTaskComment.comment,
+    partyCode: orgCode,
+    userTitle: CurrentUser.name,
+    partyName: orgShortName,
+    occuredAt: new Date(),
+  };
+
+  caseTaskComment.externalAPIExecutingParams = {
+    externalSystemCode: "ESIGN1", // код зовнішньої системи
+    externalSystemMethod: "integration/processEvent", // метод зовнішньої системи
+    data: methodData, // дані, що очікує зовнішня система для заданого методу
+    executeAsync: true, // виконувати завдання асинхронно
+  };
+}
